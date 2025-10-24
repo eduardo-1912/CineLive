9cd0ef3abb67c71cf392447b36ef3baad7f6899a
Eduardo Carvalho <ducas1912@gmail.com>
2025-10-24 11:12:33 +0100

    Web: Instalação do Template AdminLTE

--- Web/backend/views/site/index.php
+++ Web/backend/views/site/index.php
@@ -7,9 +7,12 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
+        <h1 class="display-4">Congratulations sgfgfdgfd!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <?= \hail812\adminlte\widgets\Alert::widget([
+            'type' => 'success',
+            'body' => '<h3>Congratulations!</h3>'
+        ]) ?>
 
         <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
     </div>
